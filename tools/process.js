e327c4fb8641e8ee52764fb81bea47d4945e201c
zubearc <zubear@zubear.me>
2023-09-02 03:40:37 -0400

    update data, add more condensed TSV format

--- tools/process.js
+++ tools/process.js
@@ -21,6 +21,7 @@ const pp = {
 function makeOps (agency, region) {
   const dir = join(__dirname, `../data/${agency}/${region === 'base' ? '' : region}`)
   fs.mkdirSync(dir, { recursive: true })
+  const write = (name, data) => fs.writeFileSync(join(dir, name), data)
   const writeJSON = (name, data) => fs.writeFileSync(join(dir, `${name}.json`), JSON.stringify(data, null, 2))
   const writeJSONCompact = (name, data, maxWidth = 9999) =>
     fs.writeFileSync(join(dir, `${name}.json`), compactStringify(data, { maxLength: maxWidth }))
@@ -33,8 +34,8 @@ function makeOps (agency, region) {
     for (const stop of stops) {
       updated.push({
         // ix: stop.stop_id,
-        id: stop.stop_id,
-        name: stop.stop_name,
+        id: String(stop.stop_id),
+        name: String(stop.stop_name) || '',
         description: stop.stop_desc || '',
         accessible: stop.wheelchair_boarding,
         pos: [parseFloat(stop.stop_lat), parseFloat(stop.stop_lon)],
@@ -66,12 +67,12 @@ function makeOps (agency, region) {
     const result = []
     for (const route of routes) {
       result.push({
-        id: route.route_id,
-        name: route.route_short_name,
+        id: String(route.route_id),
+        name: String(route.route_short_name) || '',
         description: route.route_long_name,
         type: route.route_type,
-        color: route.route_color,
-        textColor: route.route_text_color
+        color: String(route.route_color),
+        textColor: String(route.route_text_color)
       })
     }
     writeJSON('routes', result)
@@ -81,8 +82,15 @@ function makeOps (agency, region) {
   function handleTrips (calendar, calendarExceptions, trips, stopTimes) {
     // console.log('Processing trips', stopTimes.length, 'stop times')
     const data = processTrips(agency, region, calendar, calendarExceptions, trips, stopTimes, agency)
+
+    // Experimental generation of TSV
+    const [tsvTrips, tsvExceptions] = tripsJSON2TSV(data)
+    write('trips.tsv', tsvTrips)
+    write('trips_ex.tsv', tsvExceptions)
+
     writeJSONCompact('trips', data, 9999 + agency === 'nyct-subway' ? 9999 : 100)
 
+    // Cache some data for other calls
     for (const routeId in data.trips) {
       for (const trip of data.trips[routeId]) {
         for (const [stopId] of trip.stops) {
@@ -96,6 +104,30 @@ function makeOps (agency, region) {
   }
 
   return { handleStops: writeStops, handleShapes: writeShapes, handleRoutes, handleTrips }
+}
+
+function tripsJSON2TSV (json) {
+  const lines = []
+  for (const routeId in json.trips) {
+    for (const trip of json.trips[routeId]) {
+      const departs = []
+      for (const [dayCode, time, exceptionCode] of trip.departs) {
+        departs.push(`${dayCode},${time},${exceptionCode || ''}`)
+      }
+      const stops = []
+      for (const [stopId, secondsElapsedSinceLastArrival, loiterTime] of trip.stops) {
+        stops.push(`${stopId},${secondsElapsedSinceLastArrival},${loiterTime || ''}`)
+      }
+      lines.push([routeId, trip.sign, trip.shapeId, departs.join(';'), stops.join(';')].join('\t'))
+    }
+  }
+
+  const exceptions = []
+  for (const exception of json.exceptions) {
+    exceptions.push(`${exception.added.join(',')}\t${exception.removed.join(',')}`)
+  }
+
+  return [lines.join('\n'), exceptions.join('\n')]
 }
 
 module.exports = makeOps
